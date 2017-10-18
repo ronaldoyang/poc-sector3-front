096686cc12d8ce2adc2e4e90f18121720737ad14
Ronaldo <ronaldo.yang@matera.com>
2017-10-18 21:48:20 -0200

    Iniciando

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pocSector3FrontApp
+ * @description
+ * # pocSector3FrontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pocSector3FrontApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pocSector3FrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pocSector3FrontApp
+ */
+angular.module('pocSector3FrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
