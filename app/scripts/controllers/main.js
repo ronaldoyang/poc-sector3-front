84b7610f30ca07a3449d679e7c9bb683340d0710
Ronaldo <ronaldo.yang@matera.com>
2017-10-26 11:55:21 -0200

    Inputs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,17 @@
  * Controller of the pocSector3FrontApp
  */
 angular.module('pocSector3FrontApp')
-  .controller('MainCtrl', function () {
-
+  .controller('MainCtrl', function ($scope, gridFactory) {
+    $scope.columns = gridFactory.generateColumnsMock();
+    $scope.gridOptions = {
+      enableColumnMenu: false,
+      enableColumnMenus: false,
+      enableFiltering: false,
+      enableColumnMoving: false,
+      paginationPageSizes: [10, 100, 150, 200],
+      paginationPageSize: 10,
+      enableSorting: false, 
+      columnDefs: $scope.columns,
+      data: gridFactory.generateDataJsonMock()
+    };
   });
